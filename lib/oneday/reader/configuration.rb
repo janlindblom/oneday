58dcec5678acf3e286869d3d0272df89c7c3d878
Jan Lindblom <janlindblom@fastmail.fm>
2015-02-08 16:44:29 +0100

    And now it actually does what it's supposed to.

--- lib/oneday/reader/configuration.rb
+++ lib/oneday/reader/configuration.rb
@@ -6,27 +6,26 @@ module Oneday
       attr_accessor :config, :file
 
       def initialize(args=nil)
-        file = File.expand_path("#{ENV['HOME']}/.config/oneday.conf")
+        self.file = File.expand_path("#{ENV['HOME']}/.config/oneday.conf")
         unless args.nil?
           if args[:file]
-            file = args[:file]
+            self.file = args[:file]
           end
         end
 
-        unless File.exist? file
+        unless File.exist? self.file
           initialize_configuration_file
         end
       end
 
       def parse
-        raise file.inspect
-        config = ParseConfig.new(file)
-        config
+        self.config = ParseConfig.new(self.file)
+        self.config
       end
 
       def store
-        f = File.open(file, 'w')
-        status = config.write(f)
+        f = File.open(self.file, 'w')
+        status = self.config.write(f)
         f.close
         status
       end
@@ -34,10 +33,10 @@ module Oneday
       private
 
       def initialize_configuration_file
-        config = ParseConfig.new
-        config.add("default_storage_path", "~/Dropbox/Apps/Day One/Journal.dayone")
-        f = File.new(file, 'w')
-        status = config.write(f)
+        self.config = ParseConfig.new
+        self.config.add("default_storage_path", "~/Dropbox/Apps/Day One/Journal.dayone")
+        f = File.new(self.file, 'w')
+        status = self.config.write(f)
         f.close
         status
       end
--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,11 +1,11 @@
 require "oneday"
 
-describe Oneday::Reader::Configuration do
+describe "Oneday::Reader::Configuration" do
   it "reads configuration from a file" do
     orc = Oneday::Reader::Configuration.new
-    puts orc.file
+    expect(File.exist?(orc.file)).to eq(true)
+
     orc.parse
-    puts orc.config
     orc.store
   end
 end
